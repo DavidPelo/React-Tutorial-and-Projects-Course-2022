af07c926309227a9a12ec7eabba27932d0a4a0df
davidwpelo <65370511+DavidPelo@users.noreply.github.com>
2022-05-17 11:50:14 -0500

    Lesson 25 - 30

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,25 +3,28 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+const author = 'Charles Bukowski';
+const title = 'Ham and Rye';
+const img = 'https://images-na.ssl-images-amazon.com/images/I/91PY8Upu8oL._AC_UL254_SR254,254_.jpg';
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
+      <Book author={author} title={title} img={img} />
+      <Book author={'salinger'} title={title} img={img} />
     </section>
   );
 }
 
-const author = 'Ham and Rye';
-const Book = () => {
-  const title = 'Charles Bukowski';
+const Book = ({ author, title, img }) => {
   return (
     <article className="book">
       <img
-        src="https://images-na.ssl-images-amazon.com/images/I/91PY8Upu8oL._AC_UL254_SR254,254_.jpg"
+        src={img}
         alt="a book"
       />
       <h1>{title}</h1>
-      <h4>{author.toUpperCase()}</h4>
+      <h4>{author}</h4>
     </article>
   );
 };
