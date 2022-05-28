268724751b1b6e741a3b5d9fdf20ea2fd5f15d8c
David Pelo <david.pelo@ideaentity.com>
2022-05-28 14:54:11 -0500

    worked up to lecture 46

--- advanced-tutorial/src/tutorial/1-useState/setup/3-useState-array.js
+++ advanced-tutorial/src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useState } from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+
+  const setPeopleHandler = () => {
+    setPeople([]);
+  };
+
+  const removePerson = id => {
+    let newPeople = people.filter(person => person.id !== id);
+    setPeople(newPeople);
+  };
+
+  return (
+    <>
+      {people.map(person => {
+        const { id, name } = person;
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => removePerson(id)}>remove</button>
+          </div>
+        );
+      })}
+
+      <button className='btn' onClick={setPeopleHandler}>remove all items</button>
+    </>
+  );
 };
 
 export default UseStateArray;
--- advanced-tutorial/src/tutorial/1-useState/setup/4-useState-object.js
+++ advanced-tutorial/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerosn] = useState({
+    name: "david",
+    age: 29,
+    message: "random message",
+  });
+
+  const [name, setName] = useState("david");
+  const [age, setAge] = useState(29);
+  const [message, setMessage] = useState("random message");
+
+  const changeMessage = () => {
+    // const newPerson = { ...person, message: "hello world" };
+    // setPerosn(newPerson);
+
+    setMessage('hello world');
+  };
+
+  return (
+    <>
+      <h2>{name}</h2>
+      <h2>{age}</h2>
+      <h2>{message}</h2>
+      <button className='btn' onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
