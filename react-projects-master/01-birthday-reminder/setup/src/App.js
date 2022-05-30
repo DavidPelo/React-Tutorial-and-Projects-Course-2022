3bcd4209035d31c0db8037a6db5898c7d9cba637
davidwpelo <65370511+DavidPelo@users.noreply.github.com>
2022-05-30 13:45:27 -0500

    Completed tours project

--- react-projects-master/01-birthday-reminder/setup/src/App.js
+++ react-projects-master/01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,28 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const birthdaysToday = people.length;
+
+  const clearListHandler = () => {
+    setPeople([]);
+  };
+
+  return (
+    <main>
+      <section className='container'>
+        {people.length === 0 ? (
+          <h3>There are no birthdays :(</h3>
+        ) : (
+          <h3>{birthdaysToday} birthdays today</h3>
+        )}
+
+        <List people={people} />
+        <button onClick={clearListHandler}>Clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- react-projects-master/01-birthday-reminder/setup/src/List.js
+++ react-projects-master/01-birthday-reminder/setup/src/List.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
-  return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
+const List = ({ people }) => {
+  const renderBirthdayList = () => {
+    return people.map((person) => {
+      return (
+        <li key={person.id} className='person'>
+          <img src={person.image} alt='avatar' />
+          <div>
+            <h4>{person.name}</h4>
+            <p>{person.age} years</p>
+          </div>
+        </li>
+      );
+    });
+  };
+  
+  return <ul>{renderBirthdayList()}</ul>;
 };
 
 export default List;
